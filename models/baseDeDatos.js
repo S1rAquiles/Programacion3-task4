7146b796e61dad5fbdf7c039af6f4dc299a0562f
S1rAquiles <129690947+S1rAquiles@users.noreply.github.com>
2024-02-09 18:29:34 -0400

    Update baseDeDatos.js

--- models/baseDeDatos.js
+++ models/baseDeDatos.js
@@ -787,7 +787,7 @@ function enviarEmailRecuperacion(req,res){
  res.cookie('securityToken',token, { httpOnly: true, secure: true });
 
   // Crear la URL de recuperación de contraseña
-  const recovery = `https://michito-ni0t.onrender.com/restablecer-contrasena?token=${token}`;
+  const recovery = `https://programacion003-30-148-370.onrender.com/restablecer-contrasena?token=${token}`;
 
   // Enviar el correo electrónico de recuperación de contraseña
   const mailOptions = {
